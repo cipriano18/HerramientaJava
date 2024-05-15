df381160bb59ef9ce0adc1eb3fc43f5fd3a321c3
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2024-05-15 12:06:29 -0600

    Create the main and class person for tests

---
+++ main/src/main/java/com/proyect/Main.java
@@ -0,0 +1,9 @@
+package com.proyect;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student =new Student("cipriano", 701232432);
+        System.out.println(student.toString());
+        System.out.println("Hello world!");
+    }
+}
---
+++ main/src/main/java/com/proyect/Student.java
@@ -0,0 +1,26 @@
+package com.proyect;
+
+public class Student {
+private String name;
+private int ID;
+public Student(String name, int iD) {
+    this.name = name;
+    this.ID  = iD;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public int getID() {
+    return ID;
+}
+public void setID(int iD) {
+    ID = iD;
+}
+@Override
+public String toString() {
+    return "Student [name=" + name + ", ID=" + ID + "]";
+}
+}
