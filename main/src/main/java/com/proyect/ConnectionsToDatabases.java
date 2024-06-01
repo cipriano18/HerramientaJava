d437c85251ecf9be13c60b27fcff72b94d0e134c
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2024-06-01 08:37:58 -0600

    create the connection to the mongodb base

--- main/src/main/java/com/proyect/ConnectionsToDatabases.java
+++ main/src/main/java/com/proyect/ConnectionsToDatabases.java
@@ -2,13 +2,30 @@ package com.proyect;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Arrays;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 public class ConnectionsToDatabases {
-    public final static String user = "cipriano_12";
-    public final static String password = "cipri12";
-
+   private final static String user = "cipriano_12";
+   private final static String password = "cipri12";
+   private final static String databaseName = "prueba"; 
+   private final static String host = "127.0.0.1";
+   private final static int port = 27017;
     public static Connection connectOracle() throws SQLException {
         String url = "jdbc:oracle:thin:@//192.168.1.25:1521/XE";
         return DriverManager.getConnection(url, user, password);
+    }
+   public static MongoClient connectMongo() {
+        MongoCredential credential = MongoCredential.createCredential(user, databaseName, password.toCharArray());
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(host, port))))
+                .credential(credential)
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        return mongoClient;
     }
 }
